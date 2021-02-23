8c78903f9fa50a5936a8e91aaa33c8ff104383f8
이지수 <gemma2ee@gmail.com>
2021-02-23 11:03:19 +0900

    혈당, 복용약 데이터 추가

--- asset/js/common.js
+++ asset/js/common.js
@@ -47,4 +47,26 @@ $( function() {
       // Configuration options go here
       options: {}
   });
-aa  
+  
+
+  var ctx = document.getElementById('myChart2').getContext('2d');
+  var chart = new Chart(ctx, {
+      // The type of chart we want to create
+      type: 'line',
+  
+      // The data for our dataset
+      data: {
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+          datasets: [{
+              label: 'Blood pressure',
+              backgroundColor: 'rgb(10, 99, 132)',
+              borderColor: 'rgb(10, 99, 132)',
+              data: [0, 10, 5, 2, 20, 30, 45]
+          }]
+      },
+  
+      // Configuration options go here
+      options: {}
+  });
+
+
